dbca57f306e11fc76856a3324c350cc862c2267a
Ростислав Щербина <neozork@protonmail.com>
2025-06-26 15:43:10 +0300

    chore: Bump version to 0.4.3

--- build/lib/common/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-# src/common/__init__.py
--- build/lib/export/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/export/__init__.py
-"""
-Export module for the Shcherbyna Pressure Vector Indicator project.
-Handles exporting calculated indicator data to various formats.
-"""
-
