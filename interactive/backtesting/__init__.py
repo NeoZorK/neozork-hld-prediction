80541fdc53db2b16c1fe09ba27f6c6eeecc022c8
Ростислав Щербина <neozork@protonmail.com>
2025-09-06 22:50:56 +0300

    Add new modules to backtesting package for enhanced analysis capabilities - Included WalkForwardAnalysis, MonteCarloBacktesting, StressTesting, and PerformanceAttribution to the backtesting package. - Updated the __all__ list to reflect the new imports, improving the modularity and functionality of the backtesting framework.

--- interactive/backtesting/__init__.py
+++ interactive/backtesting/__init__.py
@@ -9,10 +9,18 @@ from .strategy_backtester import StrategyBacktester
 from .portfolio_analyzer import PortfolioAnalyzer
 from .risk_analyzer import RiskAnalyzer
 from .performance_metrics import PerformanceMetrics
+from .walk_forward_analysis import WalkForwardAnalysis
+from .monte_carlo_backtesting import MonteCarloBacktesting
+from .stress_testing import StressTesting
+from .performance_attribution import PerformanceAttribution
 
 __all__ = [
     'StrategyBacktester',
     'PortfolioAnalyzer',
     'RiskAnalyzer',
-    'PerformanceMetrics'
+    'PerformanceMetrics',
+    'WalkForwardAnalysis',
+    'MonteCarloBacktesting',
+    'StressTesting',
+    'PerformanceAttribution'
 ]
